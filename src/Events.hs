7e9c4428cfd771a6fa5c72cc3542e3925e699e97
Jonathan Daugherty <cygnus@foobox.com>
2017-08-05 15:16:15 -0700

    Resize canvas memory on terminal resize

--- src/Events.hs
+++ src/Events.hs
@@ -5,11 +5,15 @@ where
 
 import Brick
 import Lens.Micro.Platform
+import qualified Graphics.Vty as V
 
 import Types
 import Events.Main
+import Util
 
 handleEvent :: AppState -> BrickEvent Name e -> EventM Name (Next AppState)
+handleEvent s (VtyEvent (V.EvResize _ _)) =
+    continue =<< resizeCanvas s
 handleEvent s e =
     case s^.mode of
         Main -> handleMainEvent s e
