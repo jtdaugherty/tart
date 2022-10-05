7acff052b4161c72c820328530914926a84fce98
Sridhar Ratnakumar <srid@srid.ca>
2022-10-05 11:33:02 -0400

    Add nix support

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit self; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+      perSystem = { self', pkgs, ... }: {
+        haskellProjects.default = {
+          root = ./.;
+          # buildTools = hp: { fourmolu = hp.fourmolu; };
+          # source-overrides = { };
+          # overrides = self: super: { };
+          # modifier = drv: drv;
+        };
+      };
+    };
+}
